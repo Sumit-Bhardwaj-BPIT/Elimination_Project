69456c9d6f29afc1bb4fb09a9b75ed19628492ca
Sumit-Bhardwaj-BPIT <sumitbh720@gmail.com>
2023-08-30 22:57:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import data from "./data";
+import './App.css';
+import Tours from "./components/Tours";
+const App = () => {
+  const [tours,setTours]=useState(data);
+  function removeTour(id){
+    const newTours = tours.filter(tour=>tour.id!==id)
+    setTours(newTours);
+}
+
+  if (tours.length === 0) {
+    return(
+        <div className="refresh">
+          <h2>No Tours Left</h2>
+          <button className="" onClick={()=>setTours(data)} >
+            Refresh
+          </button>
+        </div>
+    );
+  }
+
+  return (
+    <div>
+        
+        <Tours tours={tours} removeTour={removeTour} ></Tours>
+    </div>
+  );
+};
+
+export default App;
